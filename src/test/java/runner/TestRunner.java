0783f8f57546b4e85f0a0511b98ad5c55cf0bab5
Sir Bailon <47519994+baiology@users.noreply.github.com>
2023-06-01 19:51:09 -0400

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,11 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/features", //the path of the feature files
-        glue={"stepDefinitions", "hooks"}, //the path of the step definition files
-        plugin= {"pretty","html:target/cucumber-html-report.html", "json:target/cucumber/Report.json"}, //to generate different types of reporting
-        monochrome = true //display the console output in a proper readable format
-//        dryRun = false //to check the mapping is proper between feature file and step def file
-        //tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
+        features = "src/test/java/features",
+        glue={"stepDefinitions", "hooks"},
+        plugin= {"pretty","html:target/cucumber-html-report.html", "json:target/cucumber/Report.json"},
+        monochrome = true
+//        dryRun =
+        //tags =
 )
 public class TestRunner {}
